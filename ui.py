f4336062ae6f86ec330ed4b4de2498493f2eae39
st2-ev <steve.s.paul22@gmail.com>
2024-01-21 01:03:34 -0800

    working ux
    Co-authored-by: Nicholas Januar <kallui@users.noreply.github.com>

--- ui.py
+++ ui.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python3
-import base64
+import asyncio
 import json
 import signal
 import time
+from pprint import pprint
 
 import cv2
 import numpy as np
+import requests
 from fastapi import Response
 from nicegui import Client, app, core, run, ui
 from ultralytics import YOLO
@@ -13,7 +14,7 @@ from ultralytics import YOLO
 from message import send_message
 
 NIKO = "+17788141068"
-# Load the YOLOv8 model
+
 model = YOLO("yolov8n.pt")
 
 class_to_label = {
@@ -103,9 +104,12 @@ label_to_class = {v: k for k, v in class_to_label.items()}
 interested_classes_labels = ["bottle", "cell phone"]
 interested_classes = [label_to_class[label] for label in interested_classes_labels]
 print(interested_classes)
-
-
+scanned_processed_objects = {}
+ui.dark_mode().enable()
 video_capture = cv2.VideoCapture(0)
+frame_cnt = 0
+scanned_objects = []
+sus_list = {}
 
 
 def convert(frame: np.ndarray) -> bytes:
@@ -113,61 +117,159 @@ def convert(frame: np.ndarray) -> bytes:
     return imencode_image.tobytes()
 
 
-frame_cnt = 0
-
-
-@app.get("/video/frame")
-# Thanks to FastAPI's `app.get`` it is easy to create a web route which always provides the latest image from OpenCV.
-async def grab_video_frame() -> Response:
-    _, frame = await run.io_bound(video_capture.read)
-
-    jpeg = await run.cpu_bound(convert, frame)
-    frame_cnt += 1
-    return Response(content=jpeg, media_type="image/jpeg")
-
-
-# For non-flickering image updates an interactive image is much better than `ui.image()`.
-video_image = ui.interactive_image().classes("w-full h-full")
-# A timer constantly updates the source of the image.
-# Because data from same paths are cached by the browser,
-# we must force an update by adding the current timestamp to the source.
-ui.timer(
-    interval=0.1, callback=lambda: video_image.set_source(f"/video/frame?{time.time()}")
-)
-
-
-async def scan(n):
-    scanned_objects = []
+@app.get("/video/home")
+async def grab_video_frame_1() -> Response:
+    global video_capture
     while video_capture.isOpened():
         success, frame = await run.io_bound(video_capture.read)
-
         if success:
             results = model(frame, classes=interested_classes, verbose=False)
 
-            scanned_objects.append(results[0].tojson())
             annotated_frame = results[0].plot()
 
-            cv2.imshow("YOLOv8 Inference", annotated_frame)
-
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
+            jpeg = await run.cpu_bound(convert, annotated_frame)
+            return Response(content=jpeg, media_type="image/jpeg")
         else:
-            break
+            raise RuntimeError("Could not read frame from video capture device.")
 
-        if frames == n:
-            break
 
-    video_capture.release()
-    cv2.destroyAllWindows()
+@app.get("/video/scan")
+async def grab_video_frame_2() -> Response:
+    global video_capture, frame_cnt, scanned_objects
+    while video_capture.isOpened():
+        success, frame = await run.io_bound(video_capture.read)
+        if success:
+            results = model(frame, classes=interested_classes, verbose=False)
+            scanned_objects.append(results[0].tojson())
 
-    objects = {}
-    for r in scanned_objects:
-        r_dict = json.loads(r)
-        for obj in r_dict:
-            if obj["name"] not in objects:
-                objects[obj["name"]] = obj
+            annotated_frame = results[0].plot()
 
-    return objects
+            jpeg = await run.cpu_bound(convert, annotated_frame)
+            return Response(content=jpeg, media_type="image/jpeg")
+        else:
+            raise RuntimeError("Could not read frame from video capture device.")
+
+
+label = ui.label("YSG")
+spinner = ui.spinner(size="xl").classes(
+    "flex justify-center items-center"
+)  # absolute-center
+spinner.set_visibility(True)
+
+ui.input(
+    label="Mobile Number",
+    placeholder="+1",
+    on_change=lambda e: result.set_text("you typed: " + e.value),
+    validation={"Input too long": lambda value: len(value) < 20},
+)
+
+video_image_1 = ui.interactive_image().classes("w-full h-full")
+ui_timer_1 = ui.timer(
+    interval=0.1,
+    callback=lambda: (video_image_1.set_source(f"/video/home?{time.time()}"),),
+)
+
+scan_button = ui.button(
+    "Scan(10s) and keep and eye",
+    on_click=lambda: scan_button_clicked(scan_button, video_image_1, ui_timer_1),
+)
+
+
+def alert_user(item):
+    # send_message(f"Somebody stole your {item}", NIKO)
+    print(f"Somebody stole your {item}")
+
+
+@app.get("/video/sentry")
+async def sentry():
+    print("WUBBALUBBADUBDUB")
+    global video_capture, sus_list, scanned_processed_objects
+    while video_capture.isOpened():
+        success, frame = await run.io_bound(video_capture.read)
+        if success:
+            results = model(frame, classes=interested_classes, verbose=False)
+            objs_in_frame = json.loads(results[0].tojson())
+            objects_in_frame = set([obj["name"] for obj in objs_in_frame])
+            scanned_objs_string = set(scanned_processed_objects.keys())
+            sub = scanned_objs_string - objects_in_frame
+            if sub:
+                for item in sub:
+                    if item not in sus_list:
+                        sus_list[item] = 1
+                    else:
+                        sus_list[item] += 1
+
+            for obj in objs_in_frame:
+                if obj["name"] in sus_list:
+                    sus_list[obj["name"]] -= 1
+                    if sus_list[obj["name"]] <= 0:
+                        del sus_list[obj["name"]]
+
+            if sus_list:
+                print(sus_list)
+                del_sus_list = []
+                for item in sus_list:
+                    if sus_list[item] >= 25:
+                        alert_user(item)
+                        del_sus_list.append(item)
+                        del scanned_processed_objects[item]
+
+                for item in del_sus_list:
+                    del sus_list[item]
+
+        jpeg = await run.cpu_bound(convert, frame)
+        return Response(content=jpeg, media_type="image/jpeg")
+
+
+def scan_button_clicked(scan_button, video_image_1, ui_timer_1):
+    global frame_cnt, scanned_objects
+    scan_button.delete()
+    video_image_1.delete()
+    ui_timer_1.delete()
+    spinner.set_visibility(False)
+    spinner.delete()
+
+    frame_cnt = 0
+    scanned_objects = []
+    video_image_2 = ui.interactive_image().classes("w-full h-full")
+    ui_timer_2 = ui.timer(
+        interval=0.1,
+        callback=lambda: video_image_2.set_source(f"/video/scan?{time.time()}"),
+    )
+
+    def post_scan(video_image_2, ui_timer_2):
+        global scanned_processed_objects
+        video_image_2.delete()
+        ui_timer_2.delete()
+
+        for r in scanned_objects:
+            r_dict = json.loads(r)
+            for obj in r_dict:
+                if obj["name"] not in scanned_processed_objects:
+                    scanned_processed_objects[obj["name"]] = obj
+
+        if len(scanned_processed_objects) > 0:
+            ui.label("Beware")
+            video_image_3 = ui.interactive_image().classes("w-full h-full")
+            video_image_3.set_source(f"static/eye.gif")
+            ui.label("This Area is Monitored")
+
+            video_image_4 = ui.interactive_image().classes("w-full h-full")
+            ui.timer(
+                interval=0.1,
+                callback=lambda: video_image_4.set_source(
+                    f"/video/sentry?{time.time()}"
+                ),
+            )
+            video_image_4.set_visibility(False)
+        else:
+            ui.label("No belongings were detected in the space")
+
+    ui.timer(
+        interval=5,
+        callback=lambda: post_scan(video_image_2, ui_timer_2),
+        once=True,
+    )
 
 
 async def disconnect() -> None:
